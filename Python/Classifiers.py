4bd6043747da9ecb00cd46d85ce7f5a7c426e476
Wojciech Kołodziejczak <wojciech.kolodziejczak97@gmail.com>
2020-10-20 16:21:49 +0200

    load and display all images, parse file names into respectable variables

--- Python/Classifiers.py
+++ Python/Classifiers.py
@@ -4,29 +4,34 @@ import os
 import cv2
 from sklearn import tree
 
-#Load images to array
+# Variables
+fig_height = 5
+fig_width = 12
+offset = 0
 path = "dataset/"
 img_list = os.listdir(path)
 img_array = []
-print(img_list)
+
 for img in img_list:
-    img_array.append(cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE))
+    # Load images into an Array
+    img_array.append(cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE))
 
-fig_height = 2
-fig_width = 4
+    # Parse file names
+    file_name, extension = os.path.splitext(img)
+    font_number, number_value = file_name.split(".")
+    img_data_list = [font_number, number_value]
 
-#Create plot
 
-fig=plt.figure()
+# Create plot
+fig = plt.figure()
 for i in range(fig_height):
-    if i==0:
-        for j in range(fig_width):
-            plt.subplot2grid((fig_height,fig_width), (i, j))
-            plt.imshow(img_array[j], cmap = "gray")
-    else:
-        for j in range(fig_width):
-            plt.subplot2grid((fig_height,fig_width), (i, j))
-            plt.imshow(img_array[j+fig_width], cmap="gray")
 
-plt.show()
+    for j in range(fig_width):
+        plt.subplot2grid((fig_height, fig_width), (i, j))
+        plt.imshow(img_array[j+offset], cmap="gray")
+    offset += fig_width
 
+print(len(img_array))
+print(img_list[199])
+print(img_array[199])
+# plt.show()
